cdbbbaeb0d138fa596411c2e8533f2378681fbf5
Selene <99739761+selenegc01@users.noreply.github.com>
2022-12-29 03:18:42 +0000

    Tercera versión de Kata3

--- src/kata3/HistogramDisplay.java
+++ src/kata3/HistogramDisplay.java
@@ -2,6 +2,7 @@ package kata3;
 
 import java.awt.Dimension;
 import javax.swing.JPanel;
+
 import org.jfree.ui.ApplicationFrame;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.JFreeChart;
@@ -12,9 +13,14 @@ import org.jfree.data.category.DefaultCategoryDataset;
 public class HistogramDisplay extends ApplicationFrame {
     
     public final Histogram<String> histogram;
+    private static final int CHART_WIDTH = 500;
+    private static final int CHART_HEIGHT = 400;
     
     public HistogramDisplay(Histogram<String> histogram){
         super("HISTOGRAMA");
+        if (histogram == null) {
+            throw new IllegalArgumentException("Histogram cannot be null");
+        }
         this.histogram = histogram;
         setContentPane(createPanel());
         pack();
@@ -28,18 +34,21 @@ public class HistogramDisplay extends ApplicationFrame {
         JFreeChart chart = ChartFactory.createBarChart("Histograma JFreeChart", "Dominios email", "Nº de emails", dataSet, PlotOrientation.VERTICAL, false, false, rootPaneCheckingEnabled);
         return chart;
     }
-    private DefaultCategoryDataset createDataSet(){
+    
+    public DefaultCategoryDataset createDataSet(){
         DefaultCategoryDataset dataSet = new DefaultCategoryDataset();
-        
-        for (String key : histogram.keySet()){
+        histogram.keySet().stream().forEach((key) -> {
             dataSet.addValue(histogram.get(key), "", key);
-        }
+        });
         return dataSet;
     }
+
     
     private JPanel createPanel(){
-        ChartPanel chartPanel = new ChartPanel(createChart(createDataSet()));
-        chartPanel.setPreferredSize(new Dimension(500,400)); 
+        DefaultCategoryDataset dataSet = createDataSet();
+        JFreeChart chart = createChart(dataSet);
+        ChartPanel chartPanel = new ChartPanel(chart);
+        chartPanel.setPreferredSize(new Dimension(CHART_WIDTH, CHART_HEIGHT)); 
         return chartPanel;
     }
 }
